968cf114f0e403cc8ce392859494069a1b166814
alexbracken <ajbracken@bsu.edu>
2023-02-25 12:34:51 -0500

    Add footer to project pages

--- elements/footer.php
+++ elements/footer.php
@@ -1,2 +1,8 @@
 </body>
+
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
+        </div>
+    </footer>
 </html>
