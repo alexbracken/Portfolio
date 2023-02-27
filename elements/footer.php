2be439bea074acc61f4c85a2f22702849855d255
alexbracken <ajbracken@bsu.edu>
2023-02-27 10:59:40 -0500

    Update footer

--- elements/footer.php
+++ elements/footer.php
@@ -1,9 +1,9 @@
 </div>
 </div>
 </body>
-    <footer class="footer level">
-        <div class="content has-text-centered level-item">
-            <p>Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
+    <footer class="footer">
+        <div class="content has-text-centered level">
+            <p class="level-item">Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
         </div>
     </footer>
 </html>
