3462faad0123de6a13561449207ead82b8bc5b4b
alexbracken <ajbracken@bsu.edu>
2023-02-27 18:37:30 -0500

    Switch localStorage to sessionStorage

--- elements/logo-animation.php
+++ elements/logo-animation.php
@@ -1,9 +1,9 @@
 <script>
 window.onload = function () {
-    if (localStorage.getItem("hasPlayed") === null) {
+    if (sessionStorage.getItem("hasPlayed") === null) {
         let loadAnimation = gsap.fromTo(".letter", {y: 30, opacity: 0}, {y: 0, opacity: 1, stagger: 0.05, ease: "elastic", duration: 2, delay: 0.5});
         loadAnimation.play()
-        localStorage.setItem("hasPlayed", true);
+        sessionStorage.setItem("hasPlayed", true);
     }
 }
 
