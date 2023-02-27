383d84af5e5a361730d598ef0e0009c9110aa587
alexbracken <ajbracken@bsu.edu>
2023-02-27 10:22:04 -0500

    Close body divs

--- elements/footer.php
+++ elements/footer.php
@@ -1,5 +1,5 @@
+</div>
 </body>
-
     <footer class="footer">
         <div class="content has-text-centered">
             <p>Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
