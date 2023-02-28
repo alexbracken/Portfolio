929046b951a23d1fda6a26228ba09fefea11b27e
alexbracken <ajbracken@bsu.edu>
2023-02-28 10:24:30 -0500

    Declare variable outside if block

--- elements/logo-animation.php
+++ elements/logo-animation.php
@@ -1,12 +1,13 @@
 <script>
+
+var loadAnimation = gsap.fromTo(".letter", {y: 30, opacity: 0}, {y: 0, opacity: 1, stagger: 0.05, ease: "elastic", duration: 2, delay: 0.5});
+
 window.onload = function () {
-    let loadAnimation = gsap.fromTo(".letter", {y: 30, opacity: 0}, {y: 0, opacity: 1, stagger: 0.05, ease: "elastic", duration: 2, delay: 0.5});
     if (sessionStorage.getItem("hasPlayed") === null) {
         loadAnimation.play()
         sessionStorage.setItem("hasPlayed", true);
     }
 }
-
 
 
 var grid = document.querySelector('.grid');
