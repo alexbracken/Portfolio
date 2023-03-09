5a3497e099e0bbfa1daef1e24a10f3a763d43d94
alexbracken <ajbracken@bsu.edu>
2023-03-09 10:20:23 -0500

    Add span, fix footer padding

--- elements/footer.php
+++ elements/footer.php
@@ -7,7 +7,7 @@
                 <p>Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
             </div>
             <div>
-                <p>This website was developed using <a href="https://automad.org" target="_blank" >Automad</a>, <a href="https://masonry.desandro.com" target="_blank">Masonry</a>, <a class="link" href="https:///greensock.com" target="_blank">GSAP</a>, and <a href="https://bulma.io" target="_blank">Bulma</a>.
+                <span><p>This website was developed using <a href="https://automad.org" target="_blank" >Automad</a>, <a href="https://masonry.desandro.com" target="_blank">Masonry</a>, <a class="link" href="https:///greensock.com" target="_blank">GSAP</a>, and <a href="https://bulma.io" target="_blank">Bulma</a>.</span>
             </div>
         </div>
     </footer>
