5fbc68daafc72f00e376433a10c8c4712604ea0f
alexbracken <ajbracken@bsu.edu>
2023-03-09 12:51:33 -0500

    Update wrappers

--- about.php
+++ about.php
@@ -1,29 +1,28 @@
 <@ elements/header.php @>
 <div class="wrapper">
-<@ elements/navbar.php @>
-
-<script>
-    window.onload = function () {
-    if (sessionStorage.getItem("aboutAnimationHasPlayed") === null) {
-        var loadAnimation = gsap.fromTo(".letter", {y: 30, opacity: 0}, {y: 0, opacity: 1, stagger: 0.05, ease: "elastic", duration: 2, delay: 0.5});
-        loadAnimation.play()
-        sessionStorage.setItem("aboutAnimationHasPlayed", true);
+    <@ elements/navbar.php @>
+    <script>
+        window.onload = function () {
+        if (sessionStorage.getItem("aboutAnimationHasPlayed") === null) {
+            var loadAnimation = gsap.fromTo(".letter", {y: 30, opacity: 0}, {y: 0, opacity: 1, stagger: 0.05, ease: "elastic", duration: 2, delay: 0.5});
+            loadAnimation.play()
+            sessionStorage.setItem("aboutAnimationHasPlayed", true);
+        }
     }
-}
-</script>
-<div class="container">
-    <section class="contact section">
-        <h1 id="name" class="title is-1" style="font-size: 0;">
-            <span class="letter">A</span>
-            <span class="letter">b</span>
-            <span class="letter">o</span>
-            <span class="letter">u</span>
-            <span class="letter">t</span>
-        </h1>
-    </section>
-    <section>
-    </section>
+    </script>
+    <div class="container">
+        <section class="contact section">
+            <h1 id="name" class="title is-1" style="font-size: 0;">
+                <span class="letter">A</span>
+                <span class="letter">b</span>
+                <span class="letter">o</span>
+                <span class="letter">u</span>
+                <span class="letter">t</span>
+            </h1>
+        </section>
+        <section>
+        </section>
 
-</div>
+    </div>
 </div>
     <@ elements/footer.php @>
