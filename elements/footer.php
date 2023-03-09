27b5f61dcf9fa63100975d84fab5ae34fd2bd2a6
alexbracken <ajbracken@bsu.edu>
2023-03-09 10:21:58 -0500

    Simplify divs

--- elements/footer.php
+++ elements/footer.php
@@ -1,14 +1,12 @@
 </div>
 </div>
 </body>
-    <footer class="footer">
-        <div class="content has-text-centered">
-            <div>
-                <p>Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
-            </div>
-            <div>
-                <span><p>This website was developed using <a href="https://automad.org" target="_blank" >Automad</a>, <a href="https://masonry.desandro.com" target="_blank">Masonry</a>, <a class="link" href="https:///greensock.com" target="_blank">GSAP</a>, and <a href="https://bulma.io" target="_blank">Bulma</a>.</span>
-            </div>
+    <footer class="footer has-text-centered">
+        <div>
+            <p>Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
+        </div>
+        <div>
+            <span><p>This website was developed using <a href="https://automad.org" target="_blank" >Automad</a>, <a href="https://masonry.desandro.com" target="_blank">Masonry</a>, <a class="link" href="https:///greensock.com" target="_blank">GSAP</a>, and <a href="https://bulma.io" target="_blank">Bulma</a>.</span>
         </div>
     </footer>
 </html>
