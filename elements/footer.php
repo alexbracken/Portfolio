508c3e4dff9809d25b4c4a2d73c076ad0a6c06f6
alexbracken <ajbracken@bsu.edu>
2023-03-09 10:16:44 -0500

    Clean footer

--- elements/footer.php
+++ elements/footer.php
@@ -2,9 +2,13 @@
 </div>
 </body>
     <footer class="footer">
-        <div class="content has-text-centered level">
-            <p class="level-item">Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
-            <p>This website was developed using <a href="https://automad.org" target="_blank" >Automad</a>, <a href="https://masonry.desandro.com" target="_blank">Masonry</a>, <a class="link" href="https:///greensock.com" target="_blank">GSAP</a>, and <a href="https://bulma.io" target="_blank">Bulma</a>.
+        <div class="content has-text-centered">
+            <div>
+                <p>Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
+            </div>
+            <div>
+                <p>This website was developed using <a href="https://automad.org" target="_blank" >Automad</a>, <a href="https://masonry.desandro.com" target="_blank">Masonry</a>, <a class="link" href="https:///greensock.com" target="_blank">GSAP</a>, and <a href="https://bulma.io" target="_blank">Bulma</a>.
+            </div>
         </div>
     </footer>
 </html>
