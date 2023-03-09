8c17bc22dcf2072920223ca9c73da6db87fd8814
alexbracken <ajbracken@bsu.edu>
2023-03-09 10:14:39 -0500

    Add credits to footer

--- elements/footer.php
+++ elements/footer.php
@@ -4,6 +4,7 @@
     <footer class="footer">
         <div class="content has-text-centered level">
             <p class="level-item">Copyright &copy; Alex Bracken, <script>document.write(new Date().getFullYear())</script></p>
+            <p>This website was developed using <a href="https://automad.org" target="_blank" >Automad</a>, <a href="https://masonry.desandro.com" target="_blank">Masonry</a>, <a class="link" href="https:///greensock.com" target="_blank">GSAP</a>, and <a href="https://bulma.io" target="_blank">Bulma</a>.
         </div>
     </footer>
 </html>
