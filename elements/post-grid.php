137a83dcc8860df85f9a16feb990aa38110720e9
alexbracken <ajbracken@bsu.edu>
2023-03-09 21:11:09 -0500

    Homepage updates

--- elements/post-grid.php
+++ elements/post-grid.php
@@ -31,40 +31,39 @@
                 <article class="project-container">
                     <div class="project-image">
                         <a href="@{url}">
-                            <@ if @{:featured} @>
-                                <@ with @{imageFeatured_01} {width: 300} @>
-                                    <@ set { ":small": @{ :fileResized } } @>
-                                <@ end @>
-                                <@ with @{imageFeatured_01} {width: 600} @>
-                                    <@ set { ":medium": @{ :fileResized } } @>
-                                <@ end @>
-                                <@ with @{imageFeatured_01} {width: 900} @>
-                                    <@ set { ":large": @{ :fileResized } } @>
-                                <@ end @>
-                                <@ with @{imageFeatured_01} {width: 1200}} @>
-                                    <@ set { "xlarge": @{ :fileResized } } @>
-                                <@ end @>
-                                
+                            <@ with @{imageFeatured_01} {width: 300} @>
+                                <@ set { ":small": @{ :fileResized } } @>
+                            <@ end @>
+                            <@ with @{imageFeatured_01} {width: 600} @>
+                                <@ set { ":medium": @{ :fileResized } } @>
+                            <@ end @>
+                            <@ with @{imageFeatured_01} {width: 900} @>
+                                <@ set { ":large": @{ :fileResized } } @>
+                            <@ end @>
+                            <@ with @{imageFeatured_01} {width: 1200}} @>
+                                <@ set { "xlarge": @{ :fileResized } } @>
+                            <@ end @>
+                            
 
-                                <@ with @{imageFeatured_01} {width: 300} @>
-                                    <img 
-                                        class="project-thumbnail"
-                                        srcset=
-                                            "@{:small} 1x,
-                                            @{:medium} 2x,
-                                            @{:large} 3x,
-                                            @{:xlarge} 4x"
-                                        src="@{ :file }" 
-                                        alt="@{ :caption }">
-                                    </img>
+                            <@ with @{imageFeatured_01} {width: 300} @>
+                                <img 
+                                    class="project-thumbnail"
+                                    srcset=
+                                        "@{:small} 1x,
+                                        @{:medium} 2x,
+                                        @{:large} 3x,
+                                        @{:xlarge} 4x"
+                                    src="@{ :file }" 
+                                    alt="@{ :caption }">
+                                </img>
+                            <@ end @>
+                            <@ if @{checkboxIsGif} @>
+                                <@ foreach in filelist @>
+                                    <@ if @{:i} >= 1 @>
+                                        <img class="project-thumbnail" src="@{:file}"><img>
+                                    <@ end @>
                                 <@ end @>
-                                <@ else @>
-                                        <@ foreach in filelist @>
-                                            <@ if @{:i} >= 1 @>
-                                                <img class="project-thumbnail" src="@{:file}"><img>
-                                            <@ end @>
-                                        <@ end @>
-                                <@ end @>
+                            <@ end @>
                         </a>
                     </div>
                     <div class="project-info">
