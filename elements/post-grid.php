a361b73dfcb73f0d47d58ceae1d1f7aa0d6592ba
alexbracken <ajbracken@bsu.edu>
2023-03-09 21:54:12 -0500

    Test logic updates

--- elements/post-grid.php
+++ elements/post-grid.php
@@ -31,7 +31,7 @@
                 <article class="project-container">
                     <div class="project-image">
                         <a href="@{url}">
-                            <@ if @{checkboxIsGif} = 'false' @>
+                            <@ if @{:gif} = 'false' @>
                                 <@ with @{imageFeatured_01} {width: 300} @>
                                     <@ set { ":small": @{ :fileResized } } @>
                                 <@ end @>
