7366eeb3f50c5b7ef2752d50cbf7ac065086e43c
alexbracken <ajbracken@bsu.edu>
2023-03-09 19:34:21 -0500

    Layout styling updates

--- page.php
+++ page.php
@@ -10,7 +10,7 @@ such as the header, navbar and footer markup.
 		{ "isFeatured": @{ checkboxIsFeatured | def("false")} } @>
 	<div class="project-page container is-widescreen">
 		<div class="columns section is-desktop">
-			<div class="column is-3 is-4-desktop">
+			<div class="column is-3-desktop">
 				<section class="page-header">
 					<h1 class="title">@{ title }</h1>
 					<div class="content">
